35cc60db6e61387565686a8b44e395e6c2977796
FernasG <fernandogoul7@gmail.com>
2024-04-12 00:42:23 -0300

    fix: solve bug with nest-cli and views

--- src/controllers/home/home.controller.ts
+++ src/controllers/home/home.controller.ts
@@ -1,8 +1,10 @@
+import { Public } from '@guards';
 import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class HomeController {
   @Get()
+  @Public()
   @Render('Home')
   public async home() {
     return { message: 'string' };
