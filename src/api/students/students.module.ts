d1ab422b984408634ee1d3758ce10aed9e830d16
FernasG <fernandogoul7@gmail.com>
2024-04-16 01:27:24 -0300

    feat: add students crud

--- src/api/students/students.module.ts
+++ src/api/students/students.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentsService } from './students.service';
 import { StudentsController } from './students.controller';
+import { Students } from '@database';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Students])],
   controllers: [StudentsController],
   providers: [StudentsService],
 })
--- src/api/students/students.service.ts
+++ src/api/students/students.service.ts
@@ -1,26 +1,96 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStudentDto } from './dto/create-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { I18nService } from 'nestjs-i18n';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateStudentDto, UpdateStudentDto } from './students.interface';
+import { Students } from '@database';
 
 @Injectable()
 export class StudentsService {
-  create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
+  constructor(
+    @InjectRepository(Students)
+    private readonly studentsRepository: Repository<Students>,
+    private readonly i18n: I18nService,
+  ) {}
+
+  public async create(createStudentDto: CreateStudentDto) {
+    const student = await this.studentsRepository.save(createStudentDto);
+
+    if (!student) {
+      const message = this.i18n.t('students.create_student_failed');
+      throw new InternalServerErrorException(message);
+    }
+
+    const message = this.i18n.t('students.create_student_success');
+
+    return { message, student };
   }
 
-  findAll() {
-    return `This action returns all students`;
+  public async findAll() {
+    return this.studentsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} student`;
+  public async findOne(id: string) {
+    const student = await this.studentsRepository.findOneBy({ id });
+
+    if (!student) {
+      const message = this.i18n.t('students.student_not_found');
+      throw new NotFoundException(message);
+    }
+
+    return { student };
   }
 
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return `This action updates a #${id} student`;
+  public async update(id: string, updateStudentDto: UpdateStudentDto) {
+    const student = await this.studentsRepository.findOneBy({ id });
+
+    if (!student) {
+      const message = this.i18n.t('students.student_not_found');
+      throw new NotFoundException(message);
+    }
+
+    if (!Object.keys(updateStudentDto).length) {
+      const message = this.i18n.t('system.invalid_request');
+      throw new BadRequestException(message);
+    }
+
+    const updateResult = await this.studentsRepository.update(
+      id,
+      updateStudentDto,
+    );
+
+    if (!updateResult) {
+      const message = this.i18n.t('students.update_student_failed');
+      throw new InternalServerErrorException(message);
+    }
+
+    const message = this.i18n.t('students.update_student_success');
+
+    return { message, student };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} student`;
+  public async remove(id: string) {
+    const student = await this.studentsRepository.findOneBy({ id });
+
+    if (!student) {
+      const message = this.i18n.t('students.student_not_found');
+      throw new NotFoundException(message);
+    }
+
+    const deleteResult = await this.studentsRepository.softDelete(id);
+
+    if (!deleteResult) {
+      const message = this.i18n.t('students.remove_student_failed');
+      throw new InternalServerErrorException(message);
+    }
+
+    const message = this.i18n.t('students.remove_student_success');
+
+    return { message };
   }
 }
