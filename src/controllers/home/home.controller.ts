ac87ed95390803787cffa595d20afeb647c298f3
Caio-Duque <caiowyrda@gmail.com>
2024-05-26 05:39:51 -0300

    add users

--- src/controllers/home/home.controller.ts
+++ src/controllers/home/home.controller.ts
@@ -1,12 +1,28 @@
 import { Public } from '@guards';
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render} from '@nestjs/common';
 
 @Controller()
 export class HomeController {
+  
   @Get()
   @Public()
   @Render('Home')
   public async home() {
     return { message: 'string' };
   }
+
+  @Get('signup')
+  @Public()
+  @Render('signup')
+  public async signup() {
+    return { message: 'string' };
+  }
+  
+  //trocar
+  @Get('dashboard')
+  @Render('dashboard')
+  public async dashboard() {
+    return { message: 'string' };
+  }
+
 }
