264438d4dbe3b75b095afe646832334fbc6be0bd
FernasG <fernandogoul7@gmail.com>
2024-05-26 22:59:10 -0300

    feat: add school-days entities

---
+++ src/database/entities/school-days.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class SchoolDays {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({ type: 'smallint', unsigned: true })
+  day: number;
+
+  @Column({ type: 'smallint', unsigned: true })
+  month: number;
+
+  @Column({ type: 'integer', unsigned: true })
+  year: number;
+
+  @Column({ type: 'date' })
+  date: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp', nullable: true })
+  deleted_at: Date;
+}
